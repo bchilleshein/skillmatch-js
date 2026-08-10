4c48bedc64a4c0675d4afbaa93f771cbea00d7c2
Bianca Hilleshein <bianca.hilleshein@indicium.tech>
2026-08-09 21:07:35 -0300

    feat: adiciona o perfil do candidato, as vagas e as recomendações de estudo

---
+++ skillmatch.js
@@ -0,0 +1,47 @@
+const candidateProfile = {
+    name: "Bianca Carolina Hilleshein",
+    interestArea: "Desenvolvimento Front-End",
+    experienceTime: "6 meses",
+    skills: [
+        "SQL", "Power BI", "dbt", "Databricks", "Noção de Banco de Dados", "Git/GitHub", "Lógica de Programação", "ETL"
+    ]
+};
+
+const jobOpenings = [
+    { 
+        company: "Tech Solutions",
+        position: "Desenvolvedor Front-End Júnior",
+        modality: "Híbrido",
+        requirements: [
+            "Lógica de Programação", "HTML5", "CSS3", "JavaScript ES6", "Git/GitHub", "CSS Responsive (Flexbox/Grid)"
+        ]
+    },
+    { 
+        company: "Dev Studios",
+        position: "Desenvolvedor React Júnior",
+        modality: "Home Office",
+        requirements: [
+            "JavaScript ES6", "React", "HTML5", "CSS3", "Consumo de APIs REST (Fetch/Async-Await)", "Git/GitHub"
+        ]
+    },
+    { 
+        company: "CodeFront",
+        position: "Desenvolvedor Front-End Web Júnior",
+        modality: "Híbrido",
+        requirements: [ 
+            "HTML5", "CSS3", "JavaScript ES6", "TypeScript", "Git/GitHub", "Tailwind CSS", "Testes Unitários (Jest)" 
+        ]
+    } 
+];
+
+const studyRecommendationsLinks = {
+    "JavaScript ES6": "https://www.freecodecamp.org/learn/javascript-v9/",
+    "HTML5": "https://developer.mozilla.org/pt-BR/docs/Web/HTML",
+    "CSS3": "https://developer.mozilla.org/pt-BR/docs/Web/CSS",
+    "React": "https://react.dev/learn",
+    "TypeScript": "https://www.typescriptlang.org/docs/",
+    "CSS Responsive (Flexbox/Grid)": "https://css-tricks.com/snippets/css/a-guide-to-flexbox/",
+    "Consumo de APIs REST (Fetch/Async-Await)": "https://developer.mozilla.org/pt-BR/docs/Web/API/Fetch_API/Using_Fetch",
+    "Tailwind CSS": "https://tailwindcss.com/docs",
+    "Testes Unitários (Jest)": "https://jestjs.io/docs/getting-started"
+};
